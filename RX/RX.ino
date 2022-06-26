39422b5940461f40198fd60cf07daad43f62aea0
FtXivan(Ванекек) <49199557+votin306@users.noreply.github.com>
2022-06-26 11:20:11 +0300

    Add files via upload

---
+++ RX/RX.ino
@@ -0,0 +1,45 @@
+#include <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
+#include <Servo.h>
+Servo myservo;
+Servo motor;
+RF24 radio(7, 8);  // "создать" модуль на пинах 9 и 10 Для Уно
+//RF24 radio(9,53); // для Меги
+
+byte address[][6] = {"1Node", "2Node", "3Node", "4Node", "5Node", "6Node"}; //возможные номера труб
+
+void setup() {
+  myservo.attach(6);
+  motor.attach(3);
+  Serial.begin(9600);         // открываем порт для связи с ПК
+  radio.begin();              // активировать модуль
+  radio.setAutoAck(1);        // режим подтверждения приёма, 1 вкл 0 выкл
+  radio.setRetries(0, 15);    // (время между попыткой достучаться, число попыток)
+  radio.enableAckPayload();   // разрешить отсылку данных в ответ на входящий сигнал
+  radio.setPayloadSize(32);   // размер пакета, в байтах
+
+  radio.openReadingPipe(1, address[0]);   // хотим слушать трубу 0
+  radio.setChannel(0x60);     // выбираем канал (в котором нет шумов!)
+
+  radio.setPALevel (RF24_PA_MAX);   // уровень мощности передатчика. На выбор RF24_PA_MIN, RF24_PA_LOW, RF24_PA_HIGH, RF24_PA_MAX
+  radio.setDataRate (RF24_250KBPS); // скорость обмена. На выбор RF24_2MBPS, RF24_1MBPS, RF24_250KBPS
+  //должна быть одинакова на приёмнике и передатчике!
+  //при самой низкой скорости имеем самую высокую чувствительность и дальность!!
+
+  radio.powerUp();        // начать работу
+  radio.startListening(); // начинаем слушать эфир, мы приёмный модуль
+}
+
+void loop() {
+  byte pipeNo;
+  int gotByte[3];
+  while (radio.available(&pipeNo)) {        // слушаем эфир со всех труб
+    radio.read(&gotByte, sizeof(gotByte));  // чиатем входящий сигнал
+    gotByte[1] = map(gotByte[1], -50, 50, 0, 50);
+    gotByte[0] = map(gotByte[0], -110, 110, 0, 180);
+    myservo.write(gotByte[1]);
+    motor.write(gotByte[0]);
+    Serial.println(gotByte[1]);
+  }
+}
